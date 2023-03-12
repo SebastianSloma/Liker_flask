7e8c91be3f706f6b1c8fb266797421507b4d6c7a
Sebastian <sebastiansloma@gmail.com>
2023-03-12 10:46:50 +0100

    add database model

--- web/__init__.py
+++ web/__init__.py
@@ -4,9 +4,15 @@ from os import path
 from flask_login import LoginManager
 
 
+db = SQLAlchemy()
+DB_NAME = 'databse.db'
+
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "123456789"
+    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -14,4 +20,21 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import User
+
+    create_databse(app)
+
+    login_manager - LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+
+def create_databse(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
